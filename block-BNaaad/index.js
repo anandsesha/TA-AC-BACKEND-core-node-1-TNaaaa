7efc5df8ca04439e5b027bd9756bdbe5bbdf1acf
Anand Seshadri <78548214+anandsesha@users.noreply.github.com>
2023-08-17 21:28:03 -0400

    basic node asg

---
+++ block-BNaaad/index.js
@@ -0,0 +1,10 @@
+// - Create a file named index.js with console.log('Welcome to Node.js') as script and run it using script processor mode (`node index.js`)
+// - add a for loop to sum integers from 1 to 10 in index.js and rerun the script.
+
+console.log('Welcome to Node.js');
+
+let sum = 0;
+for (let i = 0; i <= 10; i++) {
+  sum = sum + i;
+}
+console.log(sum);
